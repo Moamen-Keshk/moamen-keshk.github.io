95b68f10424dad03f262bb705f73410447067bf7
Moamen <moamen@users-MacBook-Air.local>
2024-02-10 09:34:41 +0200

    notification MenuAnchor

--- flutter_academy/lib/app/courses/widgets/dashboard_nav.widget.dart
+++ flutter_academy/lib/app/courses/widgets/dashboard_nav.widget.dart
@@ -5,6 +5,11 @@ import 'package:flutter_academy/app/courses/res/responsive.res.dart';
 import 'package:flutter_academy/app/users/view_models/theme_mode.vm.dart';
 import 'package:flutter_academy/main.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:badges/badges.dart' as badges;
+
+enum SampleItem { itemOne, itemTwo, itemThree }
+
+SampleItem? selectedMenu;
 
 class DashboardNav extends StatelessWidget {
   const DashboardNav({super.key});
@@ -15,6 +20,7 @@ class DashboardNav extends StatelessWidget {
       title: const Text('Flutter Academy'),
       elevation: kIsWeb ? 0 : null,
       centerTitle: kIsWeb ? false : null,
+      backgroundColor: const Color.fromARGB(255, 241, 236, 245),
       actions: (MediaQuery.of(context).size.width <= ScreenSizes.md)
           ? [
               Consumer(builder: (context, ref, child) {
@@ -29,7 +35,7 @@ class DashboardNav extends StatelessWidget {
           : [
               TextButton(
                 style: TextButton.styleFrom(
-                  foregroundColor: Colors.white,
+                  foregroundColor: Colors.grey,
                 ),
                 onPressed: () {
                   routerDelegate.go('/');
@@ -38,7 +44,7 @@ class DashboardNav extends StatelessWidget {
               ),
               TextButton(
                 style: TextButton.styleFrom(
-                  foregroundColor: Colors.white,
+                  foregroundColor: Colors.grey,
                 ),
                 onPressed: () {
                   routerDelegate.go('/courses');
@@ -50,7 +56,7 @@ class DashboardNav extends StatelessWidget {
                   routerDelegate.go('/watchlist');
                 },
                 style: TextButton.styleFrom(
-                  foregroundColor: Colors.white,
+                  foregroundColor: Colors.grey,
                 ),
                 child: const Text("Watchlist"),
               ),
@@ -58,7 +64,7 @@ class DashboardNav extends StatelessWidget {
                 final themeModeVM = ref.watch(themeModeProvider);
                 return TextButton(
                   style: TextButton.styleFrom(
-                    foregroundColor: Colors.white,
+                    foregroundColor: Colors.grey,
                   ),
                   onPressed: () {
                     themeModeVM.toggleThemeMode();
@@ -68,6 +74,35 @@ class DashboardNav extends StatelessWidget {
                       : "Dark Theme"),
                 );
               }),
+              MenuAnchor(
+                builder: (BuildContext context, MenuController controller,
+                    Widget? child) {
+                  return IconButton(
+                    onPressed: () {
+                      if (controller.isOpen) {
+                        controller.close();
+                      } else {
+                        controller.open();
+                      }
+                    },
+                    icon: const badges.Badge(
+                      badgeContent: Text('3'),
+                      badgeStyle: badges.BadgeStyle(
+                        badgeColor: Colors.blue,
+                      ),
+                      child: Icon(Icons.notifications_outlined),
+                    ),
+                  );
+                },
+                menuChildren: List<MenuItemButton>.generate(
+                  3,
+                  (int index) => MenuItemButton(
+                    onPressed: () =>
+                        setState(() => selectedMenu = SampleItem.values[index]),
+                    child: Text('Item ${index + 1}'),
+                  ),
+                ),
+              ),
               Consumer(builder: (context, ref, child) {
                 return IconButton(
                     icon: const Icon(Icons.exit_to_app),
@@ -79,4 +114,6 @@ class DashboardNav extends StatelessWidget {
             ],
     );
   }
+
+  setState(SampleItem Function() param0) {}
 }
