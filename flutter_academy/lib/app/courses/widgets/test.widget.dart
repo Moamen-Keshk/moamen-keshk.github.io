9e688326297159e4132dcd3647f3667da5b06d5c
Admin <admin@Admins-MacBook-Air.local>
2024-11-15 10:26:02 +0000

    sizing

--- flutter_academy/lib/app/courses/widgets/test.widget.dart
+++ flutter_academy/lib/app/courses/widgets/test.widget.dart
@@ -4,6 +4,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:month_year_picker/month_year_picker.dart';
 
+final formatter = DateFormat('MMM');
+
 class SingleRowMonthCalendar extends ConsumerStatefulWidget {
   const SingleRowMonthCalendar({super.key});
 
@@ -35,75 +37,90 @@ class _SingleRowMonthCalendarState
     final selectedMonth = ref.watch(selectedMonthVM);
     _daysInMonth = _getDaysInMonth(selectedMonth.year, selectedMonth.month);
     return Column(children: [
-      Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10),
-        child: SizedBox(
-            height: 100,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: _daysInMonth.length,
-              itemBuilder: (context, index) {
-                DateTime day = _daysInMonth[index];
-                DateTime today = DateTime.now();
-                bool isToday = day.day == today.day &&
-                    day.month == today.month &&
-                    day.year == today.year;
-
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 26.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        DateFormat.E()
-                            .format(day), // Short weekday name (e.g., Mon, Tue)
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      const SizedBox(height: 4),
-                      Container(
-                        height: 42,
-                        width: 42,
-                        decoration: BoxDecoration(
-                          color: isToday ? Colors.blue : Colors.grey[200],
-                          shape: BoxShape.circle,
-                        ),
-                        padding: const EdgeInsets.all(12),
-                        child: Center(
-                            child: Text(
-                          '${day.day}',
-                          style: TextStyle(
-                            color: isToday ? Colors.white : Colors.black,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        )),
-                      ),
-                    ],
-                  ),
-                );
+      Row(children: [
+        Column(children: [
+          Center(
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  shape: BeveledRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              )),
+              onPressed: () async {
+                ref.read(selectedMonthVM.notifier).state =
+                    (await showMonthYearPicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(20),
+                  lastDate: DateTime(2027),
+                ))!;
+                setState(() {});
               },
-            )),
-      ),
-      Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            ref.read(selectedMonthVM.notifier).state =
-                (await showMonthYearPicker(
-              context: context,
-              initialDate: DateTime.now(),
-              firstDate: DateTime(20),
-              lastDate: DateTime(2027),
-            ))!;
-            setState(() {});
-          },
-          child: Text(
-            '${selectedMonth.year}-${selectedMonth.month.toString().padLeft(2, '0')}',
-            style: TextStyle(fontSize: 18),
+              child: Text(
+                DateFormat('MMMM yyyy').format(selectedMonth).toString(),
+                style: TextStyle(fontSize: 15),
+              ),
+            ),
           ),
-        ),
-      )
+          Text('Rooms',
+              style: TextStyle(
+                fontSize: 16.0, // Font size
+                color: Colors.grey, // Text color
+              )),
+        ]),
+        Expanded(
+            child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: SizedBox(
+                height: 70,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: _daysInMonth.length,
+                  itemBuilder: (context, index) {
+                    DateTime day = _daysInMonth[index];
+                    DateTime today = DateTime.now();
+                    bool isToday = day.day == today.day &&
+                        day.month == today.month &&
+                        day.year == today.year;
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 26.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            DateFormat.E().format(
+                                day), // Short weekday name (e.g., Mon, Tue)
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          Container(
+                            height: 42,
+                            width: 42,
+                            decoration: BoxDecoration(
+                              color: isToday ? Colors.blue : Colors.grey[200],
+                              shape: BoxShape.circle,
+                            ),
+                            padding: const EdgeInsets.all(12),
+                            child: Center(
+                                child: Text(
+                              '${day.day}',
+                              style: TextStyle(
+                                color: isToday ? Colors.white : Colors.black,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            )),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                )),
+          ),
+        ]))
+      ])
     ]);
   }
 }
